5ddc3965f8b6a36f6d14e41bb803847ca04ba1b0
Luke Angove <luke.angove@gmail.com>
2021-11-30 00:17:47 +1100

    Add extra logging for subscriptions

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,8 +10,10 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, db: web::Data<PgPool>) -> HttpResponse {
+    let request_id = Uuid::new_v4();
     log::info!(
-        "Saving '{}' '{}' details to database as a new subscriber",
+        "request_id {}: Saving '{}' '{}' details to database as a new subscriber",
+        request_id,
         form.email,
         form.name
     );
@@ -30,11 +32,14 @@ pub async fn subscribe(form: web::Form<FormData>, db: web::Data<PgPool>) -> Http
 
     match res {
         Ok(_) => {
-            log::info!("Saved subscriber details to database");
+            log::info!(
+                "request_id {}: Saved subscriber details to database",
+                request_id
+            );
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            log::info!("Failed to execute query {:?}", e);
+            log::info!("request_id {}: Failed to execute query {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
