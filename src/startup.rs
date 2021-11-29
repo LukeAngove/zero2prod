517a538bfa4feaa8e45586032c9e4859de293e5b
Luke Angove <luke.angove@gmail.com>
2021-11-29 23:13:10 +1100

    Add initial logging

--- src/startup.rs
+++ src/startup.rs
@@ -2,6 +2,7 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use actix_web::middleware::Logger;
 
 use crate::routes::health_check;
 use crate::routes::subscribe;
@@ -10,6 +11,7 @@ pub fn run(listener: TcpListener, db: PgPool) -> Result<Server, std::io::Error> 
     let db = web::Data::new(db);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health-check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db.clone())
